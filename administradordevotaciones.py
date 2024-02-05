6d7018439c28eb58b09ae8bbea7bb145c0831d90
ximegiron08 <158303514+ximegiron08@users.noreply.github.com>
2024-02-05 10:59:45 -0600

    Add files via upload

--- administradordevotaciones.py
+++ administradordevotaciones.py
@@ -3,9 +3,38 @@ cantidad=len(dpi)
 if cantidad ==13:
     print("los partidos de presidencia a votar son: 1) partido rojo, 2) partido azul")
     presidente=input("ingrese el nombre del partido por el cual quiere votar:")
+    if presidente=="partido rojo":
+        print("votó rojo")
+    elif presidente=="partido azul":
+            print("votó azul")
+    else:
+        print("voto invalido")
+
     print("los partidos de alcalde a votar son: 1) partido amarillo, 2) partido morado, 3) partido verde, 4) partido naranja")
     alcalde=input("ingrese el nombre del partido por el cual quiere votar:")
+    if alcalde=="partido amarillo":
+        print("votó amarillo")
+    elif alcalde=="partido morado":
+        print("votó morado")
+    elif alcalde=="partido verde":
+        print("votó verde")
+    elif alcalde=="partido naranja":
+        print("votó naranja")
+    else:
+        print("voto invalido")
+        alcalde="invalido"
+        presidente="invalido"
+
+
     print("tu persona con dpi",dpi, "para presidente votarás por", presidente," y para alcalde votarás por", alcalde)
+    respuesta=input("quiere guardar sus votos? si o no") 
+
+    
+    if respuesta=="si":
+        print("se guardó su voto")
+    if respuesta=="no":
+        print("votos no guardados")
+
 else:
     print("dpi invalido")
     print("intente de nuevo")
