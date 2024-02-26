842629f9b8244dab9cf3cccd57c45ac670dd7504
ximegiron08 <158303514+ximegiron08@users.noreply.github.com>
2024-02-26 11:15:33 -0600

    Add files via upload

---
+++ e2estructurasdecontrol.py
@@ -0,0 +1,7 @@
+for numero in range(10):
+    print(numero)
+limite=int(input("ingrese un numero limite:"))
+contador=1
+while contador<=limite:
+    print(contador)
+    contador+=1
---
+++ e3convertidormedidas.py
@@ -0,0 +1,6 @@
+cm=float(input("ingrese los centimetros a convertir:"))
+
+pulgadas= cm*3937007874
+pies=pulgadas/12
+print("la longitud en pulgadas es:", pulgadas)
+print("la longitud en pies es:", pies)
