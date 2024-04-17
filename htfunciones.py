a524d2ff0e9181dd8c452168c64f4f4a249dfc74
ximegiron08 <158303514+ximegiron08@users.noreply.github.com>
2024-04-17 11:13:07 -0600

    Add files via upload

---
+++ htfunciones.py
@@ -0,0 +1,21 @@
+#ejercicio 1
+
+def nombre_de_la_funcion():
+# código quue raliza la tarea
+    pass
+
+
+
+def saludar(nombre):
+    print( f"¡Hola, {nombre} bienvenido a python!")
+
+
+saludar("Ana") #imprime ¡hola Ana, bienvenido a python!
+
+#ejercicio 2
+
+def sumar (a,b):
+    return a + b 
+
+resultado = sumar(5,3)
+print(resultado) #imprime:8
